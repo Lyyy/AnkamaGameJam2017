b242b7922e009350e336429ec2f312cc70ef3dfb
Florian Chatet <fchatet@ankama.com>
2017-12-15 22:51:56 +0100

    Question 8

---
+++ Assets/Code/Question 8/Wind.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+
+    public float strength;
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (string.Equals(col.gameObject.name, "Player"))
+        {
+            col.GetComponent<Rigidbody2D>().AddForce(Vector2.left * (GetComponent<BoxCollider2D>().size.x - Vector2.Distance(transform.position, col.transform.position)) * strength * Time.fixedDeltaTime);
+        }
+    }
+}
