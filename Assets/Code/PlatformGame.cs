b96410cf74d7da5e6057b68821e6a8088afe8a9c
Lulu <lulucinda.blog@gmail.com>
2017-12-16 01:26:57 +0100

    Add animation to Zebulon

--- Assets/Code/PlatformGame.cs
+++ Assets/Code/PlatformGame.cs
@@ -7,7 +7,14 @@ public class PlatformGame : Game
     private bool canJump = true;
     private float previousVelocityY = 0f;
 
+    private Animator animator;
+
     private readonly ContactFilter2D filter = new ContactFilter2D().NoFilter();
+
+    void Start ()
+    {
+        animator = player.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
 	protected virtual void Update ()
@@ -15,11 +22,16 @@ public class PlatformGame : Game
 	    var x = Input.GetAxis("Horizontal");
 		var velocity = player.velocity;
         velocity.x = Time.deltaTime * speed * x;
+        if (Mathf.Approximately(velocity.x, 0f))
+            animator.SetBool("Running", false);
+        else
+            animator.SetBool("Running", true);
 	    var isTouching = player.IsTouching(filter);
         if (Input.GetButtonDown("Jump") && canJump && Mathf.Abs(velocity.y) < 1f && isTouching)
         {
             canJump = false;
             velocity.y = jump * -Mathf.Sign(Physics2D.gravity.y);
+            animator.SetTrigger("Jumping");
         } 
         else if (velocity.y - previousVelocityY > -Mathf.Sign(Physics2D.gravity.y) || Mathf.Approximately(velocity.y, 0f))
 	        canJump = true;
