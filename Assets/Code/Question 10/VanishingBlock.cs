3f66acee7fc3071af6aeeb116696b83055ce708f
Florian Chatet <fchatet@ankama.com>
2017-12-16 03:09:00 +0100

    Question 10

---
+++ Assets/Code/Question 10/VanishingBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VanishingBlock : MonoBehaviour {
+
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        if (string.Equals(col.gameObject.name, "Player"))
+        {
+            StartCoroutine(Explode());
+        }
+    }
+
+    private IEnumerator Explode()
+    {
+        foreach (var col in GetComponents<BoxCollider2D>())
+            col.enabled = false;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        var scale = transform.localScale;
+        for (var i = 0.25f; i > 0f; i -= Time.deltaTime)
+        {
+            scale.x = i * 4f;
+            scale.y = scale.x;
+            transform.localScale = scale;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
