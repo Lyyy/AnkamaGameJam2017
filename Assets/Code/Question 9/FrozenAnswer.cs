615e0cb050bcad79d0c4e2b06baaf43ff7d175e2
Florian Chatet <fchatet@ankama.com>
2017-12-16 02:08:16 +0100

    Question 9.2

---
+++ Assets/Code/Question 9/FrozenAnswer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FrozenAnswer : AnswerBlock
+{
+    public Color color;
+    private bool done = false;
+
+    protected override void OnCollisionEnter2D(Collision2D col)
+    {
+        if (string.Equals(col.gameObject.name, "Player") && !done)
+        {
+            done = true;
+            spriteRenderer.color = color;
+        }
+    }
+
+    protected override void OnCollisionExit2D(Collision2D col)
+    {
+        
+    }
+}
---
+++ Assets/Code/Question 9/MultipleTimeAnswer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MultipleTimeAnswer : AnswerBlock
+{
+    private int hitNeeded = 5;
+    private int hitLeft = 5;
+
+    protected override void OnCollisionEnter2D(Collision2D col)
+    {
+        if (string.Equals(col.gameObject.name, "Player"))
+        {
+            if(--hitLeft == 0)
+                Game.GetInstance().Answer(GetComponentInChildren<Text>().text);
+            var color = Color.white * (hitLeft * 1f / hitNeeded);
+            color.a = 1f;
+            spriteRenderer.color = color;
+        }
+    }
+
+    protected override void OnCollisionExit2D(Collision2D col)
+    {
+
+    }
+}
