c870ae9fa6955e1cfab1af393c383176c6082ed0
Florian Chatet <fchatet@ankama.com>
2017-12-16 01:37:48 +0100

    Handle flip y player with gravity

--- Assets/Code/PlatformGame.cs
+++ Assets/Code/PlatformGame.cs
@@ -7,31 +7,24 @@ public class PlatformGame : Game
     private bool canJump = true;
     private float previousVelocityY = 0f;
 
-    private Animator animator;
-
     private readonly ContactFilter2D filter = new ContactFilter2D().NoFilter();
-
-    void Start ()
-    {
-        animator = player.GetComponent<Animator>();
-    }
 
     // Update is called once per frame
 	protected virtual void Update ()
 	{
+	    var scale = player.transform.localScale;
+	    scale.y = -Mathf.Sign(Physics2D.gravity.y);
+	    player.transform.localScale = scale;
 	    var x = Input.GetAxis("Horizontal");
 		var velocity = player.velocity;
         velocity.x = Time.deltaTime * speed * x;
-        if (Mathf.Approximately(velocity.x, 0f))
-            animator.SetBool("Running", false);
-        else
-            animator.SetBool("Running", true);
+	    player.GetComponent<Animator>().SetBool("Running", !Mathf.Approximately(velocity.x, 0f));
 	    var isTouching = player.IsTouching(filter);
         if (Input.GetButtonDown("Jump") && canJump && Mathf.Abs(velocity.y) < 1f && isTouching)
         {
             canJump = false;
             velocity.y = jump * -Mathf.Sign(Physics2D.gravity.y);
-            animator.SetTrigger("Jumping");
+            player.GetComponent<Animator>().SetTrigger("Jumping");
         } 
         else if (velocity.y - previousVelocityY > -Mathf.Sign(Physics2D.gravity.y) || Mathf.Approximately(velocity.y, 0f))
 	        canJump = true;
--- Assets/Code/Question 9/LaMerNoireBlock.cs
+++ Assets/Code/Question 9/LaMerNoireBlock.cs
@@ -5,7 +5,7 @@ public class LaMerNoireBlock : AutoDestroyingBlock
 
     public GameObject next;
 
-    protected virtual void OnCollisionEnter2D(Collision2D col)
+    protected override void OnCollisionEnter2D(Collision2D col)
     {
         base.OnCollisionEnter2D(col);
         if (string.Equals(col.gameObject.name, "Player"))
